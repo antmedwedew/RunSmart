33a33f8d4512613e53210a047f7a82c417416029
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-03-25 23:21:00 +0300

    fix slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.slider__inner').slick({
-        prevArrow: `<button type="button" class="slick-prev"><img src="../icons/slick_prev.svg" alt=""></button>`,
-        nextArrow: `<button type="button" class="slick-next"><img src="../icons/slick_next.svg" alt=""></button>`,
+        prevArrow: `<button type="button" class="slick-prev"><img src="./icons/slick_prev.svg" alt="prev"></button>`,
+        nextArrow: `<button type="button" class="slick-next"><img src="./icons/slick_next.svg" alt="next"></button>`,
         // autoplay: true,
         // autoplaySpeed: 2000,
         responsive: [

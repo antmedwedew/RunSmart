d5f7be5f3874b225f187bfd6ec52c04565b066ad
Anton Medwedew <medwedew.anton2017@yandex.ru>
2020-04-03 21:57:37 +0300

    add PHP mailer

--- src/js/script.js
+++ src/js/script.js
@@ -87,4 +87,20 @@ $(document).ready(function(){
 
     //validate mask-input
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //php mailer
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val('');
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
